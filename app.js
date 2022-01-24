b665351b6a35c5c97d922ee42f8c66f68c43a426
krupali pilgulwar <rpkrupali@gmail.com>
2022-01-24 17:45:42 -0600

    providing q through inquirer and getting answers from user for multiple projects

--- app.js
+++ app.js
@@ -7,13 +7,81 @@ const inquirer = require('inquirer');
 //     if(err) throw err;
 //     console.log('Portfolio complete! Check out index.html to see the output!');
 // });
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'what is your name?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'what is your github repo name?'
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'provide some information about yourself'
+        }
+    ]);
+}
 
-inquirer
-.prompt([
-    {
-        type: 'input',
-        name: 'name',
-        message: 'what is your name?'
+const promtProject = portfolioData  => {
+    // If there's no 'projects' array property, create one
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
     }
-])
-.then(answers => console.log(answers));
+    console.log(`
+        ==================================
+        ADD A NEW PROJECT
+        ==================================
+    `);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project. (Required)'
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
+        }
+    ])
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if(projectData.confirmAddProject){
+            return promtProject(portfolioData);
+        }
+        else
+            return portfolioData;
+    });
+}
+
+promptUser()
+.then(promtProject)
+.then(portfolioData => console.log(portfolioData));
